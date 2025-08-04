f43897b42ea4ba3f7ee1aa81cbe882d1c6cef48b
Agustin Odetti MBAS <aguode55@hotmail.com>
2025-08-04 11:30:11 -0300

    Config para poder ejecutar desde localhost y render.

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin: 'https://registro-asistencia-mern.onrender.com'
+  origin: ['https://registro-asistencia-mern.onrender.com', 
+    'http://localhost:3000']
 }));
 app.use(express.json());
 
